62141291b8bcb695449681b66e15714765d1c8d9
bbreaker <bbreaker@usgs.gov>
2017-08-25 15:04:56 -0500

    updates

--- R/getBasinPts.R
+++ R/getBasinPts.R
@@ -1,5 +1,21 @@
-getBasinPts <- function(basin) {
+getBasinPts <- function(site, basin) {
   
+  basin <- sp::spTransform(basin, sp::CRS("+init=epsg:4326"))
   
+  drnArea <- (rgeos::gArea(basin)/1e6) * 0.3861
+  
+  basin <- sp::spTransform(basin, sp::CRS("+proj=longlat +datum=NAD83"))
+  
+  if (drnArea <= 5) {
+    
+    crds <- rgeos::gCentroid(basin)@coords
+    
+  }
+  
+  else if (drnArea > 5 & drnArea <= 50) {
+    
+    
+    
+  }
   
 }
